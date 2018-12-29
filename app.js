3367c461541b703e495d82334fbb3b7a9e676827
musebe <emusebe9@gmail.com>
2018-12-29 23:10:55 +0000

    Configured Registration of users

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 
 const app = express();
@@ -18,6 +21,31 @@ mongoose.connect(db, {
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// Express session
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 // Routes
 app.use('/', require('./routes/index.js'));
